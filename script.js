caad4dd3aadb19bf2641f669837bf816c38c8d1a
basedmilz <basedmilz@gmail.com>
2021-02-16 17:39:09 -0500

    first changes

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+
+//Length and Number Slider
+var passLength = document.getElementById ("passwordlength");
+var passnum = document.getElementById ("passwordlengthnum");
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(characterlength, includeUppercase, includeNumbers, includeSymbols);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+// Prompt
+
+generateBtn.addEventListener("click", function() {
+  var lengthPrompt = window.prompt("Length of password");
+})
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
