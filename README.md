09c08a75317663efc7e524bc8979f47607b35a93
basedmilz <basedmilz@gmail.com>
2021-02-16 22:51:39 -0500

    Final push

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# pass-gen-mh
-This is a password generator created by Milton Harper
+# PASSWORD GENERATOR
+
+This is a webpage that password generator application created to keep your information confidential for a safe login experience.
+
+On inputting attributes that you would like your password to contain , my application will print out a unique/randomized password you can you for confidentiality when creating a account.
+
+The motivation behind this project is to create passwords that are randomized to create safer experiences as well as showcase my skill as a web developer. As I work on this project I learned different html tags and javascript formulas to show different results.
--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 // Assignment Code
+
+// Variables
+var allch = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var symbol = !"!@#$%^&*()_+";
+var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
+var passwordgen = "";
+var passLength = 8
 
+var length = document.querySelector("#passlength");
+var uppercasin = document.querySelector("#upper");
+var lowercasin = document.querySelector("#lower");
+var numberin= document.querySelector("#num")
+var symbolin = document.querySelector("#sym")
 
-
-
-//Length and Number Slider
-var passLength = document.getElementById ("passwordlength");
-var passnum = document.getElementById ("passwordlengthnum");
-
-// Write password to the #password input
+// Password Generator
 function writePassword() {
-  var password = generatePassword(characterlength, includeUppercase, includeNumbers, includeSymbols);
-  var passwordText = document.querySelector("#password");
+  passwordgen = "";
+  
+
+  for (var i = 0; i < passLength; i++) {
+    var password = Math.floor(Math.random() * allch.length);
+    
+  }
 
   passwordText.value = password;
 
 }
 
-// Prompt
 
-generateBtn.addEventListener("click", function() {
-  var lengthPrompt = window.prompt("Length of password");
-})
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword );
+
