43d46f57aed57fd883edd90bd02e9dd0ab46b86a
basedmilz <basedmilz@gmail.com>
2021-02-25 18:42:07 -0500

    Update

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var ADDnum;
-var ADDlow;
-var ADDup;
-var ADDsym;
-var Final;
+
+// Varibles for Window confirms
+var addNum ="";
+var addLow ="";
+var addUp= "";
+var addSym ="";
+var final = [];
+
 
 // Variables
 nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -12,54 +15,48 @@ lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "
 upLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 sym = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+"]
 // Write password to the #password input
-function writePassword() {
+console.log ("Gen")
+
+function generatePassword() {
   // Prompts Question length
   var question = window.prompt("How long would you like your password? Min:8 Max:128");
-
-  if (question < 8 || question > 128) {
-    window.alert("Must be within specified range (8-128 characters)")
-  }
-  else if (question === false) {
+  console.log(question)
+  if (question < 8 || question >128) {
+    window.alert("Wrong amount of characters")
     return;
-
-  }
-  else {
-    ADDnum = window.confirm('Would you like to include numbers?')
-    if (ADDnum === true) {
-      window.alert("Confirmed numbers!")
-    }
-    ADDup = window.confirm('Would you like to include uppercase?')
-    if (ADDup === true) {
-      window.alert("Confirmed uppercase!")
-    }
-    ADDlow = window.confirm('Would you like to include lowercase?')
-    if (ADDlow === true) {
-      window.alert("Confirmed lowercase!")
-    }
-    ADDsym = window.confirm('Would you like to include symbols?')
-    if (ADDsym === true) {
-      window.alert("Confirmed symbols!")
-    }
-  }
-  if (ADDlow === true) {
-    Final = lowLetters;
-  }
-  if (ADDsym === true) {
-    Final = sym;
-  }
-  if (ADDup === true) {
-    Final = upLetters;
-    if (ADDnum === true) {
-      Final = nums;
-    }
-  }
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  } 
+  console.log("Wemadeit")
+  addNum = window.confirm("Would you like to include numbers?")
+  if (addNum === true ) { window.alert('Confirmed Numbers!') }
+  addUp = window.confirm("Would you like to include uppercase?")
+  if (addUp === true ) { window.alert('Confirmed Uppercase!') }
+  addLow = window.confirm("Would you like to include lowercase?")
+  if (addLow === true ) { window.alert('Confirmed Lowercase!') }
+  addSym = window.confirm("Would you like to include symbols?")
+  if (addSym === true ) { window.alert('Confirmed Symbols!') }
+  if (addNum === true) {final = final.concat(nums)}
+console.log("addNum",final)
+if (addUp === true) {final = final.concat(upLetters)}
+console.log("addUp",final)
+if (addLow=== true) {final = final.concat(lowLetters)}
+if (addSym === true) {final = final.concat(sym)}
+console.log(final)
+var finalPassword = ""
+for (var i = 0; i < question ; i++) {
+  var index = Math.floor(Math.random() * final.length )
+  finalPassword = final[index];
+  console.log(finalPassword)
+} 
+return finalPassword
 }
 
-// Add event listener to generate button
+
+// Write password to the #password input
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 generateBtn.addEventListener("click", writePassword)
